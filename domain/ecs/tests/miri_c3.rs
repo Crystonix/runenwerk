d6927692a6675c713650d3bae7cf38852448f155
Aschenrot <128308156+aschenrot@users.noreply.github.com>
2026-09-07 22:32:17 +0200

    C6: retire messaging-specific C3 Miri cases

--- domain/ecs/tests/miri_c3.rs
+++ domain/ecs/tests/miri_c3.rs
@@ -1,8 +1,4 @@
-use ecs::{
-    BroadcastReader, BroadcastWriter, Component, Query, ResMut, Resource, Runtime,
-    TickBufferProvenance, TickBufferReader, TickBufferWriter, WorkQueueReader, WorkQueueWriter,
-    World,
-};
+use ecs::{Component, Query, ResMut, Resource, Runtime, World};
 use scheduler::ScheduleLabel;
 
 #[derive(Debug, Copy, Clone, Component)]
@@ -16,42 +12,6 @@ struct ResourceA(i32);
 
 #[derive(Debug, Copy, Clone, Resource)]
 struct ResourceB(i32);
-
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-struct MessageA(i32);
-
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-struct MessageB(i32);
-
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-struct MessageC(i32);
-
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-struct MessageD(i32);
-
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-struct MessageE(i32);
-
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-struct MessageF(i32);
-
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-struct WorkA(i32);
-
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-struct WorkB(i32);
-
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-struct WorkC(i32);
-
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-struct TickA(i32);
-
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-struct TickB(i32);
-
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-struct TickC(i32);
 
 #[derive(Copy, Clone)]
 struct C3;
@@ -141,78 +101,6 @@ fn resource_payloads_survive_other_resource_mutation_bookkeeping() {
     let mut world = World::new();
     world.insert_resource(ResourceA(1));
     world.insert_resource(ResourceB(2));
-    let mut runtime = Runtime::new();
-    runtime.add_systems::<C3, _, _>(&mut world, system);
-    runtime.run_schedule::<C3>(&mut world).unwrap();
-}
-
-#[test]
-fn broadcast_reader_reference_survives_other_stream_map_growth() {
-    fn system(
-        reader: BroadcastReader<MessageA>,
-        mut writer_b: BroadcastWriter<MessageB>,
-        mut writer_c: BroadcastWriter<MessageC>,
-        mut writer_d: BroadcastWriter<MessageD>,
-        mut writer_e: BroadcastWriter<MessageE>,
-        mut writer_f: BroadcastWriter<MessageF>,
-    ) {
-        let retained = reader.iter_all().next().unwrap();
-        writer_b.send(MessageB(retained.0));
-        writer_c.send(MessageC(retained.0));
-        writer_d.send(MessageD(retained.0));
-        writer_e.send(MessageE(retained.0));
-        writer_f.send(MessageF(retained.0));
-        assert_eq!(retained.0, 7);
-    }
-
-    let mut world = World::new();
-    world.publish_broadcast(MessageA(7));
-    let mut runtime = Runtime::new();
-    runtime.add_systems::<C3, _, _>(&mut world, system);
-    runtime.run_schedule::<C3>(&mut world).unwrap();
-}
-
-#[test]
-fn work_reader_reference_survives_other_queue_map_growth() {
-    fn system(
-        reader: WorkQueueReader<WorkA>,
-        mut writer_b: WorkQueueWriter<WorkB>,
-        mut writer_c: WorkQueueWriter<WorkC>,
-    ) {
-        let retained = reader.peek().unwrap();
-        writer_b.enqueue(WorkB(retained.0)).unwrap();
-        writer_c.enqueue(WorkC(retained.0)).unwrap();
-        assert_eq!(retained.0, 8);
-    }
-
-    let mut world = World::new();
-    world.work_queue_enqueue(WorkA(8)).unwrap();
-    let mut runtime = Runtime::new();
-    runtime.add_systems::<C3, _, _>(&mut world, system);
-    runtime.run_schedule::<C3>(&mut world).unwrap();
-}
-
-#[test]
-fn tick_reader_reference_survives_other_buffer_map_growth() {
-    fn system(
-        reader: TickBufferReader<TickA>,
-        mut writer_b: TickBufferWriter<TickB>,
-        mut writer_c: TickBufferWriter<TickC>,
-    ) {
-        let retained = reader.iter_current().next().unwrap();
-        writer_b
-            .push_current(TickB(retained.0))
-            .expect("tick B should accept the message");
-        writer_c
-            .push_current(TickC(retained.0))
-            .expect("tick C should accept the message");
-        assert_eq!(retained.0, 9);
-    }
-
-    let mut world = World::new();
-    world
-        .push_buffer_message_for_current_tick(TickBufferProvenance::UNSPECIFIED, TickA(9))
-        .unwrap();
     let mut runtime = Runtime::new();
     runtime.add_systems::<C3, _, _>(&mut world, system);
     runtime.run_schedule::<C3>(&mut world).unwrap();
