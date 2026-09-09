1350bd184bd53b8fe09a7f68aad3b32709276c5a
Aschenrot <128308156+aschenrot@users.noreply.github.com>
2026-09-09 02:11:42 +0200

    C7: remove ownership prelude exports

--- domain/ecs/src/prelude.rs
+++ domain/ecs/src/prelude.rs
@@ -1,6 +1,5 @@
 pub use crate::{
     Added, BatchCommands, Bundle, Changed, Commands, Component, ComponentState, DeferredCommand,
-    Entity, IntoSystem, IntoSystemConfigs, IntoSystemSetKey, Orphaned, OwnerId, OwnerRole,
-    OwnerState, Query, QueryOrphaned, Res, ResMut, ResView, Resource, Runtime, StatefulComponent,
-    SystemConfigExt, With, Without, World,
+    Entity, IntoSystem, IntoSystemConfigs, IntoSystemSetKey, Orphaned, Query, QueryOrphaned, Res,
+    ResMut, ResView, Resource, Runtime, StatefulComponent, SystemConfigExt, With, Without, World,
 };
